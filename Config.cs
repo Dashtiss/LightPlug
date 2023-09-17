2b7c32c7707c4afd7b461966fd5185a1a5ef2f53
Dashtiss <brandontisserand@outlook.com>
2023-09-17 18:36:05 -0500

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,10 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+public class Config : IConfig
+{
+    public bool IsEnabled { get; set; } = true;
+    public bool Debug { get; set; }
+    public string RLDisabledMessage { get; set; } = "Round Lock is off";
+    public string RLEnabledMessage { get; set; } = "Round lock is on";
+}
---
+++ LightPlug.cs
@@ -0,0 +1,46 @@
+﻿using Exiled.API.Features;
+using System;
+using Exiled.Permissions.Extensions;
+using MEC;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LightPlug
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Name { get; } = "LightPlug";
+        public override string Prefix { get; } = "LP";
+        public override string Author { get; } = "Dashtiss";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override void OnEnabled()
+        {
+            Timing.RunCoroutine(Timer());
+            base.OnEnabled();
+
+        }
+
+        public override void OnDisabled()
+        {
+            base.OnDisabled();
+        }
+        public IEnumerator<float> Timer()
+        {
+            if (Round.IsLocked)
+            {
+                foreach (Player pl in Player.List.Where(pl => pl.CheckPermission("et.roundlockinfo")))
+                {
+                    pl.Broadcast(3, Config.RLEnabledMessage, Broadcast.BroadcastFlags.AdminChat);
+                }
+            }
+            else
+            {
+                foreach (Player pl in Player.List.Where(pl => pl.CheckPermission("et.roundlockinfo")))
+                {
+                    pl.Broadcast(3, Config.RLDisabledMessage, Broadcast.BroadcastFlags.AdminChat);
+                }
+            }
+            yield return Timing.WaitForSeconds(1f);
+        }
+    }
+}
